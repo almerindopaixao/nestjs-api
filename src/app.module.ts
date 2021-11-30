b8320cf845bcc05b61e190df02f4c5bc0c8b642c
Almerindo Paixão <almerindo_paixao@live.com>
2021-11-30 15:54:23 -0300

    refactor: add mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { join } from 'path';
 import { Order } from './orders/entities/order.entity';
 
 @Module({
   imports: [
     OrdersModule,
     SequelizeModule.forRoot({
-      dialect: 'sqlite',
-      host: join(__dirname, 'database.sqlite'),
+      dialect: 'mysql',
+      host: 'db',
+      port: 3306,
+      database: 'fin',
+      username: 'root',
+      password: 'root',
       models: [Order],
       autoLoadModels: true,
     }),
